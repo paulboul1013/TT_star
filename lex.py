58a4c491bee5b0f7a18da2d95dff94bf5c4b83ae
paulboul1013 <paulboul1013@gmail.com>
2026-05-27 09:26:37 +0800

    Add Token_Type

--- lex.py
+++ lex.py
@@ -1,3 +1,5 @@
+import enum
+
 class Lexer:
     def __init__(self,source):
         self.source=source+'\n' # add a newline simplify lex/parse the last token/statement
@@ -63,5 +65,35 @@ class Token:
         self.text=token_text
         self.kind=token_kind
 
+class Token_Type(enum.Enum):
+    EOF=-1
+    NEWLINE=0
+    NUMBER=1
+    IDENT=2
+    STRING=3
 
+    #keywords
+    LABEL=101
+    GOTO=102
+    PRINT = 103
+    INPUT=104
+    LET = 105
+    IF =106
+    THEN=107
+    ENDIF=108
+    WHILE=109
+    REPEAT = 110
+    ENDWHILE=111
 
+    #operators
+    EQ=201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
